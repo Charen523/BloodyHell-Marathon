96f38828dfbbfa5529a8eca3cbf83a6f20d3536d
Euronia12 <lkh0647@naver.com>
2024-06-19 17:43:06 +0900

    [feat] 골인과 카운트다운
    isFirst로 1등 도착여부 확인
    딕셔너리로 랭킹과 플레이어 R코드 저장
    카운트다운 코루틴으로 리타이어 남은시간 표시  이후 랭킹팝업 활성화

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
-
-    public bool isPlaying = false;
-    public bool isFirst = false;
-    public List<Player> players = new List<Player>();
+    //레이스 플레이 여부
+    public bool isRacePlaying = false;
+    //랭킹 팝업 띄우기
+    [SerializeField]
+    private GameObject rankPopUp;
+    //첫번째로 도착한 유저 여부
+    public bool isFirst = true;
+    //완주한 플레이어 랭킹
+    public Dictionary<int, string> dicRank = new Dictionary<int, string>();
+    //랭킹 순위
+    private int rankIndex = 0;
+    //1등 골인 후 대기시간
     [SerializeField]
     private int waitTime = 10;
     // Start is called before the first frame update
@@ -24,14 +32,31 @@ public class GameManager : Singleton<GameManager>
 
     public void GoalIn(Player player)
     {
-
+        if (isFirst)
+        {
+            isFirst = false;
+            StartCoroutine(CountDown());
+            dicRank.Add(++rankIndex, player.name);
+        }
+        else if(isRacePlaying) 
+        {
+            dicRank.Add(++rankIndex, player.name);
+        }
+        else
+        {
+            //플레이어 리타이어
+        }
     }
 
     private IEnumerator CountDown()
     {
         for (int i = waitTime; i > 0; i--)
         {
-            yield return new WaitForSeconds(i);
+            //TODO : 남은시간 보여주는 텍스트이미지 및 애니메이션효과
+            // = waitTime.ToString();
+            yield return new WaitForSeconds(1);
         }
+        isRacePlaying = false;
+        rankPopUp.SetActive(true);
     }
 }
