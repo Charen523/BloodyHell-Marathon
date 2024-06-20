96f81aaead2627f304e743c0ea4a196852643abf
ss-zun <zxq0158@gmail.com>
2024-06-20 16:28:17 +0900

    [update] 스폰영역을 타일맵으로 받도록 변경, 게임오브젝트 AutoCollider추가

--- Assets/Scripts/Item/ItemSpawner.cs
+++ Assets/Scripts/Item/ItemSpawner.cs
@@ -1,20 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ItemSpawner : MonoBehaviour
 {
-    public GameObject SpawnRange;
-    private BoxCollider2D rangeCollider;
+    public Tilemap SpawnRange;
 
     public float SpawnTime = 1f;
     public int MaxItemsOnMap = 10;
     private int CurItemsOnMap = 0;
-
-    private void Awake()
-    {
-        rangeCollider = SpawnRange.GetComponent<BoxCollider2D>();
-    }
 
     private void Start()
     {
@@ -23,13 +18,15 @@ public class ItemSpawner : MonoBehaviour
 
     private Vector2 ReturnRandPos()
     {
-        Vector2 min = rangeCollider.bounds.min;
-        Vector2 max = rangeCollider.bounds.max;
+        BoundsInt bounds = SpawnRange.cellBounds;
+        Vector3Int min = bounds.min;
+        Vector3Int max = bounds.max;
 
-        float randomX = Random.Range(min.x, max.x);
-        float randomY = Random.Range(min.y, max.y);
-        Vector2 randomPos = new Vector2(randomX, randomY);
-
+        int randomX = Random.Range(min.x, max.x);
+        int randomY = Random.Range(min.y, max.y);
+        Vector3 worldPos = SpawnRange.CellToWorld(new Vector3Int(randomX, randomY, 0));
+        Vector2 randomPos = new Vector2(worldPos.x, worldPos.y);
+        
         return randomPos;
     }
 
