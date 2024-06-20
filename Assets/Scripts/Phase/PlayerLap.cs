3c9acd6d57e85a21d5f19520ce03492506d20ff9
Euronia12 <lkh0647@naver.com>
2024-06-20 15:01:11 +0900

    [move] PlayerLap

--- Assets/Scripts/Phase/PlayerLap.cs
+++ Assets/Scripts/Phase/PlayerLap.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class PlayerPhase
+public class PlayerLap
 {
     [Header("플레이어 정보")]
     public string playerCode;
 
     [Header("플레이어 레이스 정보")]
     public int playerRank;
-    public int racePhase;
+    public int playerScore;
+    public int raceLap;
     public bool[] checkPoints;
-    public float[] raceTime;
+    public bool retire = true;
 }
