8fc689f626854e00eafca2b4ebebc5d030e5d075
LeeSangSoos <105085706+LeeSangSoos@users.noreply.github.com>
2024-06-20 10:25:36 +0900

    add/RoomManager
    플레이어가 참여하면 현재 방의 플레이어 목록을 보여주고 다모이면 시작할 룸 씬과 스크립트 추가 중

--- Assets/LSS/Photon/Launcher.cs
+++ Assets/LSS/Photon/Launcher.cs
@@ -3,12 +3,13 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
 	#region Private Serializable Fields
 	/// <summary>
-	/// The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created.
+	/// 룸당 최대 인원 수. 이를 넘으면 참여 불가
 	/// </summary>
 	[Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
 	[SerializeField]
@@ -16,56 +17,69 @@ public class Launcher : MonoBehaviourPunCallbacks
 	#endregion
 	#region Private Fields
 	/// <summary>
-	/// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+	/// 게임 버전. 이게 같은 유저끼리 만나진다고 한다.
 	/// </summary>
 	string gameVersion = "1";
 	#endregion
+	#region Public Fields
+	//룸에 연결하는 버튼
+	[Tooltip("The Ui Panel to let the user enter name, connect enter Room")]
+	[SerializeField]
+	private GameObject controlPanel;
+
+	//로딩창
+	[Tooltip("The UI Label to inform the user that the connection is in progress")]
+	[SerializeField]
+	private GameObject progressLabel;
+	#endregion
 	#region MonoBehaviour CallBacks
-	
+
 	private void Awake()
 	{
 		// #Critical
+		// 현재 있는 모든 유저가 같은 씬을 열도록 하는 기능
 		// this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
 		PhotonNetwork.AutomaticallySyncScene = true;
 	}
 	private void Start()
 	{
-		Connect();
+		progressLabel.SetActive(false);
+		controlPanel.SetActive(true);
 	}
+
 	#endregion
 	#region Public Methods
 	/// <summary>
 	/// Start the connection process.
 	/// - If already connected, we attempt joining a random room
 	/// - if not yet connected, Connect this application instance to Photon Cloud Network
+	/// 연결 함수
+	/// 연결 되어있으면 랜덤 룸에 연결
+	/// 연결 안되어 있으면 연결하고 다시 룸에 연결
 	/// </summary>
 	public void Connect()
 	{
+		progressLabel.SetActive(true);
+		controlPanel.SetActive(false);
 		// we check if we are connected or not, we join if we are, else we initiate the connection to the server.
+		// 연결 되어있나 확인
 		if (PhotonNetwork.IsConnected)
 		{
 			// #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+			// 랜덤한 룸에 연결
 			PhotonNetwork.JoinRandomRoom();
 		}
 		else
 		{
 			// #Critical, we must first and foremost connect to Photon Online Server.
+			// 연결 안되어있으면 현재 포톤연결세팅에 맞춰서 연결
 			PhotonNetwork.ConnectUsingSettings();
 			PhotonNetwork.GameVersion = gameVersion;
 		}
 	}
-	public override void OnJoinRandomFailed(short returnCode, string message)
-	{
-		Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
-		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-		PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-	}
-	public override void OnJoinedRoom()
-	{
-		Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
-	}
+	
 	#endregion
-	#region MnoBehaviourPunCallbacks Callbacks
+	#region MonoBehaviourPunCallbacks Callbacks
 	public override void OnConnectedToMaster()
 	{
 		Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
@@ -73,7 +87,33 @@ public class Launcher : MonoBehaviourPunCallbacks
 	}
 	public override void OnDisconnected(DisconnectCause cause)
 	{
+		progressLabel.SetActive(false);
+		controlPanel.SetActive(true);
 		Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+	}
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+		// 룸에 연결 실패. 아마도 룸이 없을 것이라고 판단하고 룸을 새로 만들어서 연결
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.PublishUserId = true;
+		roomOptions.MaxPlayers = maxPlayersPerRoom;
+		PhotonNetwork.CreateRoom(null, roomOptions);
+	}
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+		//룸에 게스트로 연결
+	}
+
+	/// <summary>
+	/// 룸을 새로 만들었을 경우 Room 씬으로 이동하고, 이 유저가 룸의 호스트
+	/// </summary>
+	public override void OnCreatedRoom()
+	{
+		base.OnCreatedRoom();
+		PhotonNetwork.LoadLevel(1);
 	}
 	#endregion
 }
---
+++ Assets/LSS/Photon/RoomManager.cs
@@ -0,0 +1,82 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+	[Header("PlayerList")]
+	[SerializeField] private GameObject playerListContent;
+	[SerializeField] private Transform playerListParent;
+
+	private int playerIdNumber = 0;
+
+	#region MonoBehaviour Callbacks
+	private void Awake()
+	{
+		// 마스터만 해당 로직 실행
+		if (!PhotonNetwork.IsMasterClient)
+		{
+			Destroy(gameObject);
+		}
+		DefaultPool pool = PhotonNetwork.PrefabPool as DefaultPool;
+		pool.ResourceCache.Add("PlayerListContent", playerListContent);
+
+	}
+
+	private void Start()
+	{
+		if (PhotonNetwork.IsMasterClient)
+		{
+			PhotonPlayerData.Instance.AddPlayer(PhotonNetwork.LocalPlayer.UserId, GetNextPlayerId());
+			MakePlayerListContent(PhotonPlayerData.Instance.PlayerIdDict[PhotonNetwork.LocalPlayer.UserId]);
+		}
+	}
+	#endregion
+	#region Private Methods
+	private IEnumerator StartGame()
+	{
+		float timer = 10f;
+		while(timer  > 0)
+		{			
+			yield return new WaitForSeconds(1f);
+			timer--;
+		}
+		PhotonNetwork.LoadLevel(2);
+	}
+	private int GetNextPlayerId()
+	{
+		return playerIdNumber++;
+	}
+	private void MakePlayerListContent(int id)
+	{
+		GameObject newListContent = PhotonNetwork.Instantiate("PlayerListContent", Vector3.zero, Quaternion.identity, 0);
+		TextMeshProUGUI tmp = newListContent.GetComponentInChildren<TextMeshProUGUI>();
+		tmp.SetText($"유저 Id : {id}");
+	}
+	#endregion
+
+	#region Public Methods
+	// 새로운 플레이어 입장시 UserId와 새 PlayerId 매칭, 리스트에 표시
+	public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+	{
+		if (PhotonNetwork.IsMasterClient)
+		{
+			base.OnPlayerEnteredRoom(newPlayer);
+			PhotonPlayerData.Instance.AddPlayer(newPlayer.UserId, GetNextPlayerId());
+
+			MakePlayerListContent(PhotonPlayerData.Instance.PlayerIdDict[newPlayer.UserId]);
+
+			//유저가 다 모이면 게임 시작
+			if (PhotonNetwork.CountOfPlayersInRooms == 5)
+			{
+				Debug.Log("유저 5명 모임, 10초 뒤 시작");
+				StartCoroutine(StartGame());
+			}
+		}
+	}
+	#endregion
+}
