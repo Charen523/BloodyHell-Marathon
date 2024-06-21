797c0a66ce4dfb62ad7b0a30342ec9f1ec1b91eb
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-21 19:46:43 +0900

    [feat] RoomName, Settings, SkipBtn, MapDropdown

--- Assets/Scripts/PhotonDev/RoomManager.cs
+++ Assets/Scripts/PhotonDev/RoomManager.cs
@@ -1,51 +1,90 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
 	#region SerializeField
-	[Header("PlayerList")]
-	[SerializeField] private GameObject playerListContentPrefab;
-	[SerializeField] private Transform playerListParent;
+	[Header("Room Gameobjects")]
+	[SerializeField] private TextMeshProUGUI roomName;
+	[SerializeField] private TMP_Dropdown mapDropdown;
+	[SerializeField] private GameObject ExitBtn;
+    [SerializeField] private GameObject skipBtn;
 
-	[Header("GameStartCounter")]
+    [Header("PlayerList")]
+	[SerializeField] private GameObject playerListContentPrefab; //없애거나 배열로 변경.
+	[SerializeField] private Transform playerListParent; //없어져도 될듯?
+
+	[Header("GameStart Counter")]
 	[SerializeField] private GameObject startCounterBG;
 	[SerializeField] private TextMeshProUGUI startCounterTxt;
+	[SerializeField] private int startCount = 10;
 	#endregion
+
 	#region Private Field
 	private Coroutine gameStartCoroutine;
 	private Dictionary<int, GameObject> playerObjectList = new Dictionary<int, GameObject>();
 	private string gameSceneName = "CharacterAnimeTestScene COPY";
-	#endregion
+    #endregion
 
-	#region MonoBehaviour Callbacks
+    #region MonoBehaviour Callbacks
 
-	private void Start()
+    private void Awake()
+    {
+        if (startCounterTxt == null)
+			startCounterTxt = startCounterBG.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
 	{
+		roomName.text = "Room. " + PhotonNetwork.CurrentRoom.Name.Substring(0, 4);
 		if (!PhotonNetwork.IsMasterClient) return;
+
 		AddPlayerToData(PhotonNetwork.LocalPlayer);
 		startCounterBG.SetActive(false);
-	}
+		skipBtn.SetActive(false);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            skipBtn.SetActive(true);
+        }
+    }
 	#endregion
 
 	#region Private Methods
 	// 게임 시작 카운트 다운. 다되면 마스터가 게임 시작
 	private IEnumerator StartGame()
 	{
-		float timer = 10f;
+		int timer = startCount;
+        
+		
+        
 		while (timer > 0)
 		{
-			startCounterTxt.SetText($"게임 시작 {timer.ToString("F0")}...");
+			startCounterTxt.SetText($"전원 준비완료!\n게임 시작 {timer}초 전...");
+			
+			
+			
 			yield return new WaitForSeconds(1f);
+			
 			timer--;
 		}
-		if (PhotonNetwork.IsMasterClient)
-		{
-			PhotonNetwork.LoadLevel(gameSceneName);
-		}
+
+		LoadGameScene();
+
+    }
+
+	private IEnumerator ShowPromptTxt(string content)
+	{
+
+
+		yield return new WaitForSeconds(1f);
+
+
 	}
 
 	// 플레이어를 화면에 표시하는 프리팹 생성
@@ -130,6 +169,25 @@ public class RoomManager : MonoBehaviourPunCallbacks
 		base.OnPlayerLeftRoom(otherPlayer);
 		RemovePlayerFromData(otherPlayer);
 	}
+
+	public void LoadGameScene()
+	{
+        if (PhotonNetwork.IsMasterClient)
+        {
+            switch (mapDropdown.value)
+            {
+                case 0:
+                    PhotonNetwork.LoadLevel(gameSceneName);
+                    break;
+                case 1:
+                    PhotonNetwork.LoadLevel("GameScene");
+                    break;
+                default:
+                    Debug.LogError("Selected Map Option is out of index.");
+                    break;
+            }
+        }
+    }
 
 	#endregion
 }
