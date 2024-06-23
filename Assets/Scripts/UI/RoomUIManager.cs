6a84ed2b0bc3fa04415a27f89f751b26ef0510f0
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-23 13:03:19 +0900

    [feat] UI와 RoomManager 스크립트 분리+MapDropdown 마스터만 변경하도록 함

--- Assets/Scripts/UI/RoomUIManager.cs
+++ Assets/Scripts/UI/RoomUIManager.cs
@@ -1,18 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
 using UnityEngine;
 
-public class RoomUIManager : MonoBehaviour
+public class RoomUIManager : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
+    #region SerializeField
+    [Header("Room Gameobjects")]
+    [SerializeField] private TextMeshProUGUI roomName;
+    [SerializeField] private TMP_Dropdown mapDropdown;
+    [SerializeField] private GameObject ExitBtn;
+    [SerializeField] private GameObject skipBtn;
+
+    [Header("PlayerList")]
+    [SerializeField] private GameObject playerListContentPrefab;
+    [SerializeField] private Transform playerListParent;
+
+    [Header("GameStart Counter")]
+    [SerializeField] private GameObject startCounterBG;
+    [SerializeField] private TextMeshProUGUI startCounterTxt;
+    [SerializeField] private int startCount = 10;
+    #endregion
+
+    public int StartCount => startCount;
+    public TMP_Dropdown MapDropdown => mapDropdown;
+
+    #region Monobehaviour Callbacks
+
+    private void Start()
     {
-        
+        roomName.text = "Room. " + PhotonNetwork.CurrentRoom.Name.Substring(0, 4);
+
+        startCounterBG.SetActive(false);
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            skipBtn.SetActive(false);
+            mapDropdown.interactable = false;
+        }
+    }
+    #endregion
+
+    public void ShowSkipBtn()
+    {
+        skipBtn.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateStartCounter(int timer)
     {
-        
+        startCounterTxt.SetText($"전원 준비완료!\n게임 시작 {timer}초 전...");
+    }
+
+    public void ShowStartCounter()
+    {
+        startCounterBG.SetActive(true);
+    }
+
+    public void HideStartCounter()
+    {
+        startCounterBG.SetActive(false);
+    }
+
+    [PunRPC]
+    public void MakePlayerListContent(int id)
+    {
+        GameObject newListContent = Instantiate(playerListContentPrefab, playerListParent);
+        TextMeshProUGUI tmp = newListContent.GetComponentInChildren<TextMeshProUGUI>();
+        tmp.SetText($"유저 Id : {id}");
+    }
+
+    [PunRPC]
+    public void RemovePlayerListContent(int id)
+    {
+        foreach (Transform child in playerListParent)
+        {
+            TextMeshProUGUI tmp = child.GetComponentInChildren<TextMeshProUGUI>();
+            if (tmp != null && tmp.text.Contains(id.ToString()))
+            {
+                Destroy(child.gameObject);
+                break;
+            }
+        }
+    }
+
+    [PunRPC]
+    public void UpdateMapDropdown(int value)
+    {
+        mapDropdown.value = value;
+    }
+
+    private void OnMapDropdownChanged(int value)
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC("UpdateMapDropdown", RpcTarget.OthersBuffered, value);
+        }
     }
 }
