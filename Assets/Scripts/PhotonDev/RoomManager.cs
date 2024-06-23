89f10699864fc6a5d65bf64d75d4d2a59b585c4e
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-23 18:56:44 +0900

    [update] 코드 리팩토링+방장 설정되는거 확인.

--- Assets/Scripts/PhotonDev/RoomManager.cs
+++ Assets/Scripts/PhotonDev/RoomManager.cs
@@ -27,18 +27,19 @@ public class RoomManager : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            InitRoom();
+            InitRoom(); 
 
             //SetCustomProperties가 비동기이기 때문.
-            yield return new WaitForSeconds(1);
-
+            yield return new WaitForSeconds(0.3f);
             AddPlayerToData(PhotonNetwork.LocalPlayer, AssignPlayerSlot()); //Master의 PlayerData 추가.
+
+            yield return new WaitForSeconds(0.3f);
             InitMaster(); //방장 권한.
         }
     }
     #endregion
 
-    #region Private Methods
+    #region Init Data
     private void InitRoom()
     {
         /*방에 존재하는 플레이어 감지 프로퍼티 초기화.*/
@@ -63,43 +64,41 @@ public class RoomManager : MonoBehaviourPunCallbacks
             Debug.LogError("LocalPlayer doesnt have indexKey CustomProperties.");
         }
     }
+    #endregion
 
+    #region Player Datas
     private void AddPlayerToData(Photon.Realtime.Player newPlayer, int index)
     {// 마스터가 처리하는 플레이어 추가
         PhotonPlayerData.Instance.AddPlayer(newPlayer.UserId, index);
-        SetPlayerIndex(index);
 
         if (PhotonNetwork.IsMasterClient)
         {
-            Debug.Log("현재 인덱스: " + index);
             photonView.RPC("AddPlayerUI", RpcTarget.AllBuffered, newPlayer.UserId, index);
         }
     }
 
     private void RemovePlayerFromData(Photon.Realtime.Player otherPlayer)
     {// 마스터가 처리하는 플레이어 제거
-        StopCoroutine(gameStartCoroutine);
-        roomUIManager.HideStartCounter();
-        PhotonPlayerData.Instance.PlayerIdDict.Remove(otherPlayer.UserId);
-
-        int playerInGameId = PhotonPlayerData.Instance.PlayerIdDict[otherPlayer.UserId];
         if (PhotonNetwork.IsMasterClient)
         {
+            PhotonPlayerData.Instance.PlayerIdDict.Remove(otherPlayer.UserId);
+            int playerInGameId = PhotonPlayerData.Instance.PlayerIdDict[otherPlayer.UserId];
             photonView.RPC("RemovePlayerUI", RpcTarget.AllBuffered, playerInGameId);
         }
     }
 
     private int AssignPlayerSlot()
     {
-        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(RoomProperties.playerSlotKey, out object values))
+        if (PhotonNetwork.CurrentRoom.CustomProperties.TryGetValue(RoomProperties.playerSlotKey, out object value))
         {
-            bool[] playerSlots = (bool[])values;
+            bool[] playerSlots = (bool[])value;
 
             for (int i = 0; i < playerSlots.Length; i++)
             {
                 if (!playerSlots[i])
                 {
                     playerSlots[i] = true;
+
                     ExitGames.Client.Photon.Hashtable newPlayerSlots
                         = new ExitGames.Client.Photon.Hashtable() { { RoomProperties.playerSlotKey, playerSlots } };
                     PhotonNetwork.CurrentRoom.SetCustomProperties(newPlayerSlots);
@@ -108,56 +107,11 @@ public class RoomManager : MonoBehaviourPunCallbacks
                     return i;
                 }
             }
-
             Debug.LogError("PlayerSlot out of index.");
             return -1;
         }
-
         Debug.LogError("Cant find playerSlotKey in CurrentRoom CustomProperties.");
         return -1;
-    }
-
-    private IEnumerator StartGame()
-    {// 게임 시작 카운트 다운. 다되면 마스터가 게임 시작
-        int timer = roomUIManager.StartCount;
-        
-        if (PhotonNetwork.IsMasterClient)
-        {
-            roomUIManager.GetSkipBtn().interactable = true;
-        }
-
-		while (timer > 0)
-		{
-            roomUIManager.UpdateStartCounter(timer);
-            yield return new WaitForSeconds(1f);
-			timer--;
-		}
-
-		LoadGameScene();
-    }
-
-	
-
-	[PunRPC]
-	private void StartCountDown()
-    {// 마스터가 인원 확인하고 시작 카운트 다운
-        Debug.Log($"유저 {PhotonPlayerData.Instance.MaxNumberOfPlayers}명 모임, 10초 뒤 시작");
-		roomUIManager.ShowStartCounter();
-		gameStartCoroutine = StartCoroutine(StartGame());
-	}
-
-    
-
-    [PunRPC]
-    private void AddPlayerUI(string userId, int index)
-    {
-        roomUIManager.ShowConnectedPlayer(userId, index);
-    }
-
-    [PunRPC]
-    private void RemovePlayerUI(int index)
-    {
-        roomUIManager.HideDisconnectedPlayer(index);
     }
 
     private void SetPlayerIndex(int index)
@@ -166,7 +120,105 @@ public class RoomManager : MonoBehaviourPunCallbacks
                                 = new ExitGames.Client.Photon.Hashtable() { { PlayerProperties.indexKey, index } };
         PhotonNetwork.LocalPlayer.SetCustomProperties(newPlayerIndex);
     }
+    #endregion
 
+    #region AllBuffered PunRPC 
+    [PunRPC]
+    private void AddPlayerUI(string userId, int index)
+    {//들어온 사람 정보 표시.
+        roomUIManager.ShowConnectedPlayer(userId, index);
+    }
+
+    [PunRPC]
+    private void RemovePlayerUI(int index)
+    {//나간 사람 UI 숨김.
+        roomUIManager.HideDisconnectedPlayer(index);
+    }
+
+    [PunRPC]
+    private void StartCountDown()
+    {// 마스터가 인원 확인하고 시작 카운트 다운
+        Debug.Log($"유저 {PhotonPlayerData.Instance.MaxNumberOfPlayers}명 모임, 10초 뒤 시작");
+        roomUIManager.ShowStartCounter();
+        gameStartCoroutine = StartCoroutine(StartGame());
+    }
+    private IEnumerator StartGame()
+    {// 게임 시작 코루틴.
+        int timer = roomUIManager.StartCount;
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            roomUIManager.GetSkipBtn().interactable = true;
+        }
+
+        while (timer > 0)
+        {
+            roomUIManager.UpdateStartCounter(timer);
+            yield return new WaitForSeconds(1f);
+            timer--;
+        }
+
+        LoadGameScene();
+    }
+
+    [PunRPC]
+    private void StopCountDown()
+    {
+        if (gameStartCoroutine != null)
+        {
+            StopCoroutine(gameStartCoroutine);
+            roomUIManager.HideStartCounter();
+        }
+    }
+    #endregion
+
+    #region Master PunRPC
+    [PunRPC]
+    private void RequestCheckReady()
+    {//모든 사람이 준비완료 되었는지 확인.
+        foreach (Photon.Realtime.Player player in PhotonNetwork.PlayerList)
+        {
+            if (player.CustomProperties.TryGetValue(PlayerProperties.readyKey, out object isReady))
+            {
+                if (!(bool)isReady) return; //준비 아님.
+            }
+            else return; //버튼 누른 적 없음.
+        }
+
+        // 모든 플레이어가 준비된 상태라면 게임 시작
+        photonView.RPC("StartCountDown", RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    private void RequestStopGame()
+    {//준비 카운트 정지.
+        roomUIManager.GetSkipBtn().interactable = false;
+        photonView.RPC("StopCountDown", RpcTarget.AllBuffered);
+    }
+    #endregion
+
+    
+
+    
+    #region UI Interacts
+    public void LoadGameScene()
+    {//skipBtn or 자동시작.
+        if (PhotonNetwork.IsMasterClient)
+        {
+            switch (roomUIManager.MapDropdown.value)
+            {
+                case 0:
+                    PhotonNetwork.LoadLevel("CharacterAnimeTestScene COPY");
+                    break;
+                case 1:
+                    PhotonNetwork.LoadLevel("GameScene");
+                    break;
+                default:
+                    Debug.LogError("Selected Map Option is out of index.");
+                    break;
+            }
+        }
+    }
     private void TogglePlayerReady()
     {
         bool isReady;
@@ -188,81 +240,14 @@ public class RoomManager : MonoBehaviourPunCallbacks
         // 마스터에게 상태 확인 요청
         if (isReady)
         {
-            photonView.RPC("RequestCheckAllPlayersReady", RpcTarget.MasterClient);
+            photonView.RPC("RequestCheckReady", RpcTarget.MasterClient);
         }
         else
         {
-            
-                photonView.RPC("StopGame", RpcTarget.MasterClient);
+
+            photonView.RPC("RequestStopGame", RpcTarget.MasterClient);
         }
     }
-
-    [PunRPC]
-    private void RequestCheckAllPlayersReady()
-    {
-        CheckAllPlayersReady();
-    }
-
-    private void CheckAllPlayersReady()
-    {
-        foreach (Photon.Realtime.Player player in PhotonNetwork.PlayerList)
-        {
-            object isReady;
-            if (player.CustomProperties.TryGetValue(PlayerProperties.readyKey, out isReady))
-            {
-                if (!(bool)isReady)
-                {
-                    return;
-                }
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        // 모든 플레이어가 준비된 상태라면 게임 시작
-        photonView.RPC("StartCountDown", RpcTarget.AllBuffered);
-    }
-
-    [PunRPC]
-    private void StopGameMaster()
-    {
-        roomUIManager.GetSkipBtn().interactable = false;
-        photonView.RPC("StopGameAll", RpcTarget.AllBuffered);
-    }
-
-    [PunRPC]
-    private void StopGameAll()
-    {
-        if (gameStartCoroutine != null)
-        {
-            StopCoroutine(gameStartCoroutine);
-            roomUIManager.HideStartCounter();
-        }
-    }
-    #endregion
-
-    #region UI Interacts
-    public void LoadGameScene()
-    {//skipBtn or 자동시작.
-        if (PhotonNetwork.IsMasterClient)
-        {
-            switch (roomUIManager.MapDropdown.value)
-            {
-                case 0:
-                    PhotonNetwork.LoadLevel("CharacterAnimeTestScene COPY");
-                    break;
-                case 1:
-                    PhotonNetwork.LoadLevel("GameScene");
-                    break;
-                default:
-                    Debug.LogError("Selected Map Option is out of index.");
-                    break;
-            }
-        }
-    }
-
     private void OnMapChanged(int value)
     {//MapDropdown
         if (!PhotonNetwork.IsMasterClient) { return; }
@@ -289,7 +274,8 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     #region MonoBehaviourPunCallbacks Callbacks
     public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
-    {// 게스트 플레이어가 추가되면 해당 플레이어의 id를 표시하고 인원수가 다 차면 게임 10초 뒤에 시작
+    // 게스트 플레이어가 추가되면 해당 플레이어의 id를 표시하고 인원수가 다 차면 게임 10초 뒤에 시작
+    {
         if (!PhotonNetwork.IsMasterClient) { return; }
 
         base.OnPlayerEnteredRoom(newPlayer);
@@ -298,17 +284,15 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     }
 
-	public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
     // 게스트 플레이어가 도중에 나가면 게임 시작을 취소하고, 해당 플레이어는 화면과 데이터상 목록에서 제외
     {
-        StopGameAll();
-        //
-
+        StopCountDown();
+        RemovePlayerFromData(otherPlayer);
+        
         if (!PhotonNetwork.IsMasterClient) { return; }
 
-		base.OnPlayerLeftRoom(otherPlayer);
         roomUIManager.GetSkipBtn().interactable = false;
-        RemovePlayerFromData(otherPlayer);
 	}
 
     public override void OnMasterClientSwitched(Photon.Realtime.Player newMasterClient)
