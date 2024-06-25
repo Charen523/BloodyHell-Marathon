130f933d28ba25925ce295f40f92815173d855cc
Euronia12 <lkh0647@naver.com>
2024-06-25 19:11:53 +0900

    [fix] 포톤뷰 ID수정

--- Assets/Scripts/Managers/RaceManager.cs
+++ Assets/Scripts/Managers/RaceManager.cs
@@ -7,8 +7,12 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 using TMPro;
+using System.Xml;
+using ExitGames.Client.Photon;
+using Photon.Pun.Demo.PunBasics;
+using System.Runtime.InteropServices;
 
-public class RaceManager : Singleton<RaceManager>
+public class RaceManager : Singleton<RaceManager>, IPunObservable
 {
     //레이스 플레이 여부
     public bool isRacePlaying = false;
@@ -19,7 +23,7 @@ public class RaceManager : Singleton<RaceManager>
     //첫번째로 도착한 유저 여부
     public bool isFirst = true;
     //플레이어 랭킹
-    private Dictionary<int, PlayerLap> dicRank = new Dictionary<int, PlayerLap>();
+    public Dictionary<int, PlayerLap> dicRank = new Dictionary<int, PlayerLap>();
     //플레이어정보 
     public Dictionary<string, PlayerLap> dicPlayer = new Dictionary<string, PlayerLap>();
     [SerializeField]
@@ -27,9 +31,10 @@ public class RaceManager : Singleton<RaceManager>
     //랭킹 순위
     private int rankIndex = 0;
 
+    private PhotonView pv;
+
     //트랙 정보
     public Track track;
-
     //1등 골인 후 대기시간
     [SerializeField]
     private int waitTime = 10;
@@ -44,11 +49,15 @@ public class RaceManager : Singleton<RaceManager>
     public UnityAction<int, PlayerLap> OnCheckPoint;
     public UnityAction<Dictionary<int, PlayerLap>> OnShowRank;
 
-    public PhotonShowBoard showBoard;
     protected override void Awake()
     {
         isDontDestroyOnLoad = false;
         base.Awake();
+        pv = GetComponent<PhotonView>();
+        if (pv.ViewID == 0)
+        {
+            pv.ViewID = 999;
+        }
     }
 
     // Start is called before the first frame update
@@ -105,36 +114,67 @@ public class RaceManager : Singleton<RaceManager>
         }
     }
 
-    [PunRPC]
     public void GoalIn(PlayerLap player)
     {
         if (isFirst)
         {
             isFirst = false;
-            showBoard.ShowBoard();
+            //showBoard.ShowBoard();
+            Debug.Log("False");
+            pv.RPC("StartCounting", RpcTarget.AllBuffered);
         }
-
         racers.Remove(player);
         dicRank.Add(++rankIndex, player);
         dicRank[rankIndex].playerRank = rankIndex;
         dicRank[rankIndex].retire = false;
+
+        // GoalIn 메서드를 RPC로 호출
+        pv.RPC("RPC_GoalIn", RpcTarget.OthersBuffered, player.playerCode);
     }
 
+    [PunRPC]
+    private void RPC_GoalIn(string playerCode)
+    {
+        if (dicPlayer.ContainsKey(playerCode))
+        {
+            PlayerLap player = dicPlayer[playerCode];
+            racers.Remove(player);
+            dicRank.Add(++rankIndex, player);
+            dicRank[rankIndex].playerRank = rankIndex;
+            dicRank[rankIndex].retire = false;
+        }
+    }
 
+    [PunRPC]
+    private void StartCounting()
+    {
+        Debug.Log("Start");
+        StartCoroutine(CountDown());
+    }
 
     public IEnumerator CountDown()
     {
+        Debug.Log("Count");
         int currentTime = waitTime;
         countdown.SetActive(true);
         for (int i = currentTime; i > 0; i--)
-        {
-            
+        {            
             countdownTxt.text = i.ToString();
             countdownBar.fillAmount = (float)i / waitTime;
             yield return new WaitForSeconds(1);
         }
-        isRacePlaying = false;
+        countdownTxt.text = 0.ToString();
+        countdownBar.fillAmount = 0;        
         yield return null;
+        SetRetireUser();
+        rankPopUp.SetActive(true);
+        OnShowRank?.Invoke(dicRank);
+    }    
+
+    private void SetRetireUser()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        isRacePlaying = false;
         for (int i = 0; i < racers.Count; i++)
         {
             dicRank.Add(++rankIndex, racers[i]);
@@ -142,9 +182,41 @@ public class RaceManager : Singleton<RaceManager>
             dicRank[rankIndex].retire = true;
             racers.Remove(racers[i]);
         }
-        rankPopUp.SetActive(true);
-        OnShowRank?.Invoke(dicRank);
     }
 
-    
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(dicRank.Count);
+            foreach (var kvp in dicRank)
+            {
+                stream.SendNext(kvp.Key);
+                stream.SendNext(kvp.Value.playerCode);
+                stream.SendNext(kvp.Value.color);
+                stream.SendNext(kvp.Value.playerScore);
+                stream.SendNext(kvp.Value.playerRank);
+                stream.SendNext(kvp.Value.retire);
+            }
+        }
+        else
+        {
+            int count = (int)stream.ReceiveNext();
+            dicRank.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                int key = (int)stream.ReceiveNext();
+                string code = (string)stream.ReceiveNext();
+                int color = (int)stream.ReceiveNext();
+                int score = (int)stream.ReceiveNext();
+                int rank = (int)stream.ReceiveNext();
+                bool retire = (bool)stream.ReceiveNext();
+                dicRank[key].playerCode = code;
+                dicRank[key].color = color;
+                dicRank[key].playerScore = score;
+                dicRank[key].playerRank = rank;
+                dicRank[key].retire = retire;
+            }
+        }
+    }
 }
